a37ef24c8403df2f7217a0bb49023f8f03425014
James Wen <jrw2175@columbia.edu>
2016-08-30 18:26:57 -0400

    WIP: Maintain buildpack packager cache after specs run

--- spec/helpers/cache_directory_helpers.rb
+++ spec/helpers/cache_directory_helpers.rb
@@ -12,4 +12,23 @@ module CacheDirectoryHelpers
   def remove_from_cache_dir(uri)
     FileUtils.rm_f(uri_to_cache_path(uri))
   end
+
+  def self.make_cache_copy(copy_location)
+    if Dir.exist?(BUILDPACK_PACKAGER_CACHE_DIR)
+      FileUtils.mv(BUILDPACK_PACKAGER_CACHE_DIR, copy_location)
+    end
+  end
+
+  def self.restore_cache_copy(copy_location)
+    if Dir.exist?(copy_location)
+      FileUtils.mv(copy_location, BUILDPACK_PACKAGER_CACHE_DIR)
+    end
+  end
+
+  def self.remove_current_cache
+    puts BUILDPACK_PACKAGER_CACHE_DIR
+    puts `ls -la /Users/pivotal/`
+    FileUtils.rm_f(BUILDPACK_PACKAGER_CACHE_DIR)
+    puts `ls -la /Users/pivotal/`
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,20 @@ RSpec.configure do |config|
   config.include FileSystemHelpers
   config.include CacheDirectoryHelpers
   config.include FakeBinaryHostingHelpers
+
+  cache_copy_location = File.join(ENV['HOME'], '.buildpack-packager', 'cache-copy')
+
+  config.before(:suite) do
+    CacheDirectoryHelpers.make_cache_copy(cache_copy_location)
+  end
+
+  config.after(:suite) do
+    puts "I RAN"
+    CacheDirectoryHelpers.remove_current_cache
+    #CacheDirectoryHelpers.restore_cache_copy(cache_copy_location)
+  end
+
+  config.before(:all) do
+    CacheDirectoryHelpers.remove_current_cache
+  end
 end
